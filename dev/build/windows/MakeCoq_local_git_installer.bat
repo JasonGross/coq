56a838bbf668496cbb0ff77aada51a39e8308f06
Jason Gross <jgross@mit.edu>
2017-05-18 14:38:57 -0400

    Add cygwin cache

--- dev/build/windows/MakeCoq_local_git_installer.bat
+++ dev/build/windows/MakeCoq_local_git_installer.bat
@@ -4,5 +4,6 @@ call MakeCoq_MinGW.bat ^
   -arch=64 ^
   -installer=Y ^
   -coqver=local ^
-  -destcyg=%ROOTPATH%\cygwin_coq64_86git_inst ^
-  -destcoq=%ROOTPATH%\coq64_86git_inst
+  -destcyg=%ROOTPATH%\cygwin_coq64_git_inst ^
+  -destcoq=%ROOTPATH%\coq64_git_inst ^
+  -cygcache=%ROOTPATH%\cygwin_coq64_git_cache
