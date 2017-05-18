ae85f1b43dda42bb5d34ecb0e622bf81f24fcb21
Jason Gross <jgross@mit.edu>
2017-05-18 14:40:39 -0400

    Move config info into appveyor.yml

--- dev/build/windows/MakeCoq_local_git_installer.bat
+++ dev/build/windows/MakeCoq_local_git_installer.bat
@@ -5,5 +5,4 @@ call MakeCoq_MinGW.bat ^
   -installer=Y ^
   -coqver=local ^
   -destcyg=%ROOTPATH%\cygwin_coq64_git_inst ^
-  -destcoq=%ROOTPATH%\coq64_git_inst ^
-  -cygcache=%ROOTPATH%\cygwin_coq64_git_cache
+  -destcoq=%ROOTPATH%\coq64_git_inst
