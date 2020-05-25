d249aa80b9ae94ba2dddf2b40c9d64467d2a5bc2
Jason Gross <jgross@mit.edu>
2020-05-25 18:50:54 -0400

    Add support for CoqMakefile.local-late
    This allows users to write rules that depend on variables such as
    VOFILES and VDFILE, which is essential in some of my use-cases (for
    example, to properly handle seamless version switching, I need to make
    all of the .vo files depend on a file where I record the version of Coq
    being used).
    
    Fixes #10912

---
+++ test-suite/coq-makefile/local-late-extension/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+. ../template/path-init.sh
+
+set -x
+coq_makefile -Q . Top foo.v -o Makefile
+rm -f .*.d *.d
+make --no-print-directory -j1 printvo 2>&1 \
+    | grep -v 'make[^:]*: warning: -jN forced in submake: disabling jobserver mode.' \
+    | grep -v 'coq_makefile -Q . Top foo.v -o Makefile' \
+    | grep -v 'make[^:]*: Entering directory' \
+    | grep -v 'make[^:]*: Leaving directory' \
+           > make-printvo.log
+diff -u make-printvo.log.expected make-printvo.log || exit $?
